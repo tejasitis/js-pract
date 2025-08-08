5982047bd66673eeb16d7fe6858731e36f9c9b2e
Tejas <tejastripathi30@gmail.com>
2025-08-08 05:48:38 +0530

    aded datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,4 +1,29 @@
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
+"use strict"; //all the code will be treated as newer version of JS
+
+//alert(3 + 3); // to run this in node , we have different syntax for it
+
+let fname = "hitesh";
+let age = 18;
+let isLoggedIn = false;
 let state;
+let f1name = "dsdfs";
+
+//primitive data types in JS
+//number => 2 to the power 53
+//bigInt => 2 to the power 64
+//string
+//boolean = true or false
+//null => this is a standalone value as well as data type , empty value
+//undefined  => variable is declared but not assigned any value
+//symbol =>used for uniqueness
+
+
+//object 
+ 
+console.log(typeof age); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
+
