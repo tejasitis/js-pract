5cc2632691ce6d9573362f162c1787a5d04a2ee0
Tejas <tejastripathi30@gmail.com>
2025-08-08 08:20:54 +0530

    .

--- basics/Data-summary.js
+++ basics/Data-summary.js
@@ -13,7 +13,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -30,12 +30,15 @@ let myObj = {
 }
 
 const myFunction = function(){
-    console.log("Hello world");
+    // console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
-
+let a = 25
+let b = a
+b = 30
+console.log(a, b);
 
