a5d1dd55f0cb573009169e1d1839626791e743e5
Tejas <tejastripathi30@gmail.com>
2025-08-15 06:30:22 +0530

    added arrow

--- basics-03/Arrow.js
+++ basics-03/Arrow.js
@@ -1,3 +1,32 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
 //different ways of writing functions in JavaScript
 // 1. Function Declaration
 function xyz(){
@@ -24,3 +53,13 @@ const pqr = ( ) =>{
 //console.log(ass(80,140));
 
 const ass = (num1 , num2) => ( num1+num2 ); //Implicit return, no need to write return 
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
